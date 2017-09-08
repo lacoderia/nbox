eaa386c4f7d3e0b914873c6e9bc7cb5c86a5da6f
tonklis <benjamin.hernandez@in2teck.com>
2017-09-08 12:53:52 -0500

    Blank Instructor test

--- app/admin/admin_schedules.rb
+++ app/admin/admin_schedules.rb
@@ -105,7 +105,7 @@ ActiveAdmin.register Schedule, :as => "Clases" do
   form do |f|
     f.inputs "Detalles de clases" do
       f.input :datetime, label: "Horario"
-      f.input :instructor, label: "Instructor", :collection => Instructor.active.collect{|i| [ "#{i.first_name} #{i.last_name}", i.id]}, :as => :select, :include_blank => false 
+      f.input :instructor, label: "Instructor", :collection => Instructor.active.collect{|i| [ "#{i.first_name} #{i.last_name}", i.id]}, :as => :select 
       f.input :room, label: "Cuarto", :collection => Room.all.collect{|room| [room.description, room.id]}, :as => :select, :include_blank => false 
       f.input :description, label: "Descripción"
       f.input :free, label: "Gratis"
--- app/serializers/schedule_serializer.rb
+++ app/serializers/schedule_serializer.rb
@@ -15,11 +15,13 @@ class ScheduleSerializer < ActiveModel::Serializer
   end
   
   def instructor
-    instructor_obj = {}
-    instructor_obj[:id] = object.instructor.id
-    instructor_obj[:first_name] = object.instructor.first_name
-    instructor_obj[:last_name] = object.instructor.last_name
-    instructor_obj
+    if object.instructor
+      instructor_obj = {}
+      instructor_obj[:id] = object.instructor.id
+      instructor_obj[:first_name] = object.instructor.first_name
+      instructor_obj[:last_name] = object.instructor.last_name
+      instructor_obj
+    end
   end
 
 end
