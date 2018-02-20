3b43b1db0765909fc4c6e5d778307138a7f11e91
tonklis <benjamin.hernandez@in2teck.com>
2018-02-20 17:12:17 -0600

    Fixed headers after updating user password and email

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,14 +14,17 @@ class UsersController < ApiController
         sign_in(@user)
 
         if @user.linked
+         
           # remote update password
           @user.remote_login_and_set_headers
           remote_valid_update = User.remote_update_account(@user.email, user_params[:password], @user.headers)
-          #local update password
-          valid_update = @user.update_account(@user.email, user_params[:password])
-          if not (valid_update and remote_valid_update.code == "200")
+
+          if remote_valid_update.code == "200"
+            @user.set_headers(Connection.get_headers remote_valid_update)
+          else
             raise 'La actualización de contraseña no pudo realizarse. Favor de ponerse en contacto con el administrador.' 
           end
+
         end
         
       end
@@ -70,16 +73,20 @@ class UsersController < ApiController
       valid_email = current_user.validate_email(params[:email])
       
       if valid_email and remote_valid_email.code == "200"
-        
+
         # synchronize user, password
+        @user = current_user
         remote_valid_update = User.remote_update_account(params[:email], params[:password], current_user.headers)
         valid_update = current_user.update_account(params[:email], params[:password]) 
-        
+ 
         if valid_update and remote_valid_update.code == "200"
-          render json: current_user, status: :ok
+          bypass_sign_in(@user)
+          @user.set_headers(Connection.get_headers remote_valid_update)
+          render json: @user, status: :ok
         else
           raise 'La sincronización de usuario y contraseña no pudo realizarse. Favor de ponerse en contacto con el administrador.' 
         end
+
       else
         raise 'El correo seleccionado tiene a un usuario que no está vinculado a esta cuenta. Favor de ponerse en contacto con el administrador.'
       end
@@ -124,9 +131,12 @@ class UsersController < ApiController
 
   def update_account
     begin
+      @user = current_user
       valid_update = current_user.update_account params[:email], params[:password]
+
       if valid_update
-        render json: current_user, status: :ok
+        bypass_sign_in(@user)      
+        render json: @user, status: :ok
       else
         raise 'Error actualizando la cuenta.'
       end
